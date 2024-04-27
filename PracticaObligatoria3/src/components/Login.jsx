afba63139019592e6c9fde801fe365b07ddb7f33
unknown <gaston.koch@hotmail.com>
2024-04-27 15:51:47 -0300

    Practica Obligatoria 3

--- PracticaObligatoria3/src/components/Login.jsx
+++ PracticaObligatoria3/src/components/Login.jsx
@@ -1,36 +1,41 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
+import propTypes from "prop-types";
 
 const Login = () => {
     const [newName, setNewName] = useState("")
 
     const handleNewName = (e) => {
-        console.log(e.target.value.toLowerCase())
-        if(e.target.value.toLowerCase().includes("o")){
+        if (e.target.value.toLowerCase().includes("o")) {
             alert("Por favor, ¡Nombres de usuario sin la letra o!")
+            setnewUserName('')
         } else {
             setNewName(e.target.value)
         }
-        
+
     }
 
     const onSubmit = (e) => {
         e.preventDefault()
-        if(newName == ""){
+        if (newName.length === 0) {
             alert("Usuario inválido para registrarse")
         } else {
             alert("¡Usuario registrado correctamente!")
             setNewName("")
         }
     }
-    
-  return (
-    <form  onSubmit={onSubmit}>
-    <label htmlFor="name">Nombre </label>
-    <input id="GET-name" type="text" name="name" value={newName} onChange={handleNewName}/>
-    <button type='submit'>Registrarse</button>
-    <p>{newName}</p>
-    </form>
-  )
+
+    return (
+        <form onSubmit={onSubmit}>
+            <label htmlFor="name">Nombre </label>
+            <input id="GET-name" type="text" name="name" value={newName} onChange={handleNewName} />
+            <button type='submit'>Registrarse</button>
+            <p>{newName}</p>
+        </form>
+    )
 }
 
 export default Login
+
+Login.propTypes = {
+    handleUsers: propTypes.func,
+}
